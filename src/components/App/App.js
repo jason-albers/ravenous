0c82d88e40306715b5340c9b134d4603d1cf9c3a
jason-albers <soup@sandwich.net>
2025-08-11 18:01:43 -0500

    Cleaned up merge conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,105 +1,37 @@
-<<<<<<< HEAD
 import React from 'react';
 import styles from './App.module.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const MOCK_DATA = [
-  {
-    imageSrc:
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-  },
-  {
-    imageSrc:
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-  },
-  {
-    imageSrc:
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-  },
-  {
-    imageSrc:
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-  },
-  {
-    imageSrc:
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90,
-  },
-];
-=======
-import React from "react";
-import styles from "./App.module.css";
-
-import BusinessList from "../BusinessList/BusinessList";
-import SearchBar from "../SearchBar/SearchBar";
-
 const business = {
   imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Bordertown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
+    'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
   rating: 4.5,
   reviewCount: 90,
 };
 
-const businesses = [business, business, business, business, business, business];
->>>>>>> 2994f465 (Completed Ravenous project step 3)
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
 
 const App = () => {
   return (
     <div className={styles.App}>
       <h1>ravenous</h1>
       <SearchBar />
-<<<<<<< HEAD
-      <BusinessList businesses={MOCK_DATA} />
-=======
       <BusinessList businesses={businesses} />
->>>>>>> 2994f465 (Completed Ravenous project step 3)
     </div>
   );
 };
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,14 @@
-import React from "react";
-import styles from "./BusinessList.module.css";
+import React from 'react';
+import styles from './BusinessList.module.css';
 
-import Business from "../Business/Business";
+import Business from '../Business/Business';
 
 const BusinessList = ({ businesses }) => {
   return (
     <div className={styles.BusinessList}>
-<<<<<<< HEAD
-      {businesses.map((business, index) => (
-        <Business key={business.name + index} business={business} />
-      ))}
-=======
       {businesses.map((business) => {
         return <Business business={business} key={business.name} />;
       })}
->>>>>>> 2994f465 (Completed Ravenous project step 3)
     </div>
   );
 };
